4ee34ac9763695cd4ace506c516fb4ffb045ac39
YSD)Tabeta <h-tabeta@yotsuba-system.co.jp>
2019-06-29 20:56:25 +0900

    [fix] 謎のエラーが発生していたのでプロジェクト作り直し

---
+++ ForecastLotoApp/app/src/main/java/com/leaf/sa/forecastlotoapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.leaf.sa.forecastlotoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
