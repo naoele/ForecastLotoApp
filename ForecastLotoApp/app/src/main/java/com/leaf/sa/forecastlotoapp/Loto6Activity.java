9bedf39bde9a91cbdd7049313b9ec47764fd46b8
YSD)Tabeta <naoele1dev@gmail.com>
2019-07-28 23:17:23 +0900

    [clean] 不要なメソッドを削除

--- ForecastLotoApp/app/src/main/java/com/leaf/sa/forecastlotoapp/Loto6Activity.java
+++ ForecastLotoApp/app/src/main/java/com/leaf/sa/forecastlotoapp/Loto6Activity.java
@@ -10,7 +10,6 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -168,22 +167,6 @@ public class Loto6Activity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        return super.onOptionsItemSelected(item);
     }
 
     /**
